3f571d731aa77add4d56bf29a4b6418749b32d32
W. Randy King <Kingwrandy@gmail.com>
2018-01-14 00:36:04 -0500

    Initial version

---
+++ myq-poly.py
@@ -0,0 +1,251 @@
+#!/usr/bin/python
+# Polglot v2 Node Server for Chamberlain LiftMaster Garage Door Openers through MyQ Cloud Service
+
+import sys
+import re
+import time
+from myqapi import MyQ, DEVICE_TYPE_GARAGE_DOOR_OPENER, DEVICE_TYPE_GATEWAY
+import polyinterface
+
+
+_ISY_OPEN_CLOSE_UOM = 79 # 0=Open, 100=Closed
+_ISY_BARRIER_STATUS_UOM = 97 # 0=Closed, 100=Open, 101=Unknown, 102=Stopped, 103=Closing, 104=Opening
+_ISY_INDEX_UOM = 25 # Index UOM for custom door states below (must match editor/NLS in profile)
+_ISY_BOOL_UOM =2 # Used for reporting status values for Controller node
+_IX_GDO_ST_CLOSED = 0
+_IX_GDO_ST_OPEN = 1
+_IX_GDO_ST_STOPPED = 2
+_IX_GDO_ST_CLOSING = 3
+_IX_GDO_ST_OPENING = 4
+_IX_GDO_ST_UNKNOWN = 9
+
+_LOGGER = polyinterface.LOGGER
+
+# Node for a garage door opener
+class GarageDoorOpener(polyinterface.Node):
+
+    id = "GARAGE_DOOR_OPENER"
+
+    # Temporarily override the start() method in the Node class
+    def start(self):
+        pass
+
+    # Open Door
+    def cmd_don(self, command):
+
+        # Place the controller in active polling mode
+        self.controller.active = True
+        self.controller.last_active = time.time()
+
+        if self.parent.myQConnection.open(self.address):
+            self.setDriver("ST", _IX_GDO_ST_OPENING)
+        else:
+            _LOGGER.warning("Call to open() failed in DON command handler.")
+
+    # Close Door
+    def cmd_dof(self, command):
+
+        # Place the controller in active polling mode
+        self.controller.active = True
+        self.controller.last_active = time.time()
+
+        if self.parent.myQConnection.close(self.address):
+            self.setDriver("ST", _IX_GDO_ST_CLOSING)
+        else:
+            _LOGGER.warning("Call to close() failed in DOF command handler.")
+
+    # Update node states
+    def query(self):
+        self.parent.update_node_states()
+        self.reportDrivers()
+
+    drivers = [{"driver": "ST", "value": _IX_GDO_ST_UNKNOWN, "uom": _ISY_INDEX_UOM}]
+    commands = {
+        "DON": cmd_don,
+        "DOF": cmd_dof
+    }
+
+# Controller class
+class Controller(polyinterface.Controller):
+
+    id = "CONTROLLER"
+
+    def __init__(self, poly):
+        super(Controller, self).__init__(poly)
+        self.name = "MyQ Connection"
+        self.myQConnection = None
+        self.active_poll = 20
+        self.inactive_poll = 60
+        self.active = False
+        self.last_active = 0
+        self.last_poll = 0
+
+    # Start the node server
+    def start(self):
+
+        _LOGGER.info("Started MyQ Node Server...")
+
+        # get service credentials from custom configuration parameters
+        try:
+            customParams = self.polyConfig["customParams"]
+            userName = customParams["username"]
+            password = customParams["password"]
+        except KeyError:
+            _LOGGER.error("Missing MyQ service credentials in configuration.")
+            raise
+
+        # get polling intervals and ttl from custom configuration parameters
+        if "tokenttl" in customParams:
+            ttl = customParams["tokenttl"]
+        else:
+            ttl = 900
+        if "activeupdateinterval" in customParams:
+            self.active_poll = customParams["activeupdateinterval"]
+        else:
+            self.active_poll = 20
+        if "inactiveupdateinterval" in customParams:
+            self.inactive_poll = customParams["inactiveupdateinterval"]
+        else:
+            self.inactive_poll = 60
+
+        # create a connection to the MyQ cloud service
+        self.myQConnection = MyQ(userName, password, ttl, _LOGGER)
+
+        # startup in active mode polling
+        currentTime = time.time()
+        self.active = True
+        self.last_active = currentTime
+
+    # Override query to perform status update from the MyQ service before reporting driver values
+    def query(self):
+
+        _LOGGER.debug("Updating node states in Controller.query()...")
+
+        # Update the node states and force report of all driver values
+        self.update_node_states(True)
+
+    # called every longPoll seconds (default 30)
+    def longPoll(self):
+
+        pass
+
+    # called every shortPoll seconds (default 10)
+    def shortPoll(self):
+
+        # if node server is not setup yet, return
+        if self.myQConnection == None:
+            return
+
+        currentTime = time.time()
+
+        # reset active flag if 5 minutes has passed
+        if self.last_active < (currentTime - 300):
+            self.active = False
+
+        # check for elapsed polling interval
+        if ((self.active and (currentTime - self.last_poll) >= self.active_poll) or
+                (not self.active and (currentTime - self.last_poll) >= self.inactive_poll)):
+
+            # update the node states
+            _LOGGER.debug("Updating node states in Controller.shortPoll()...")
+            self.update_node_states()
+
+            self.last_poll = currentTime
+
+    # update the state of all nodes from the MyQ service
+    # Parameters:
+    #   forceReport - force reporting of all driver values (for query)
+    def update_node_states(self, forceReport=False):
+
+        # get device details from myQ service
+        devices = self.myQConnection.get_device_list()
+        if devices is None:
+            _LOGGER.warning("get_device_list() returned no devices.")
+
+            # set driver values for controller to false to reflect service connection is gone
+            serviceStatus = 0
+            gatewayOnline = 0
+
+        else:
+
+            # If devices were returned, set the service status to True
+            serviceStatus = 1
+
+            for device in devices:
+
+                if device["type"] == DEVICE_TYPE_GATEWAY:
+
+                    # Update controller node state value
+                    gatewayOnline = 1 if device["online"] else 0
+
+                elif device["type"] == DEVICE_TYPE_GARAGE_DOOR_OPENER:
+
+                    # if the node doesn't exist, create it
+                    if device["id"] in self.nodes:
+                        gdoNode = self.nodes[device["id"]]
+                    else:
+                        gdoNode = GarageDoorOpener(
+                            self,
+                            self.address,
+                            device["id"],
+                            get_valid_node_name(device["description"])
+                        )
+                        self.addNode(gdoNode)
+
+                    # update the state value for the matching node
+                    value = get_st_driver_value(device["state"])
+                    gdoNode.setDriver("ST", value, True, forceReport)
+
+                    # if a device state has a door in motion, set the active mode flag  (affects polling interval)
+                    if value in [_IX_GDO_ST_CLOSING, _IX_GDO_ST_OPENING, _IX_GDO_ST_UNKNOWN]:
+                        self.active = True
+                        self.last_active = time.time()
+
+        # Update the controller node states
+        self.setDriver("GV2", serviceStatus, True, forceReport)
+        self.setDriver("GV3", gatewayOnline, True, forceReport)
+
+    drivers = [
+        {"driver": "ST", "value": 0, "uom": _ISY_BOOL_UOM},
+        {"driver": "GV1", "value": 0, "uom": _ISY_BOOL_UOM},
+        {"driver": "GV2", "value": 0, "uom": _ISY_BOOL_UOM},
+        {"driver": "GV3", "value": 0, "uom": _ISY_BOOL_UOM}
+    ]
+    commands = {"QUERY": query}
+
+
+# Converts state value from MyQ to custom door states setup in editor/NLS in profile:
+#   0=Closed, 1=Open, 2=Stopped, 3=Closing, 4=Opening, 9=Unknown
+def get_st_driver_value(state):
+    if state == "1":    # open
+        return _IX_GDO_ST_OPEN
+    elif state == "2":  # closed
+        return _IX_GDO_ST_CLOSED
+    elif state == "3":  # stopped
+        return _IX_GDO_ST_STOPPED
+    elif state == "4":  # opening
+        return _IX_GDO_ST_OPENING
+    elif state == "5":  # closing
+        return _IX_GDO_ST_CLOSING
+    elif state == "8":  # moving
+        return _IX_GDO_ST_UNKNOWN
+    elif state == "9":  # open
+        return _IX_GDO_ST_OPEN
+    else:
+        return _IX_GDO_ST_UNKNOWN
+
+# Removes invalid charaters for ISY Node description
+def get_valid_node_name(name):
+
+    # Remove <>`~!@#$%^&*(){}[]?/\;:"'` characters from names
+    return re.sub(r"[<>`~!@#$%^&*(){}[\]?/\\;:\"']+", "", name)
+
+# Main function to establish Polyglot connection
+if __name__ == "__main__":
+    try:
+        polyglot = polyinterface.Interface()
+        polyglot.start()
+        control = Controller(polyglot)
+        control.runForever()
+    except (KeyboardInterrupt, SystemExit):
+        sys.exit(0)
---
+++ myqapi.py
@@ -0,0 +1,198 @@
+# LiftMaster MyQ API wrapper class
+# Portions Copyright (c) 2017 arraylabs
+
+import sys
+import time
+import logging
+import requests
+
+_APP_ID = "Vj8pQggXLhLy0WHahglCD4N1nAkkXQtGYpq2HrHD7H1nvmbT55KqtN6RSF4ILB/i"
+_HOST_URI = "myqexternal.myqdevice.com"
+_LOGIN_ENDPOINT = "api/v4/User/Validate"
+_DEVICE_LIST_ENDPOINT = "api/v4/UserDeviceDetails/Get"
+_DEVICE_SET_ATTR_ENDPOINT = "api/v4/DeviceAttribute/PutDeviceAttribute"
+_DEVICE_GET_ATTR_ENDPOINT = "api/v4/DeviceAttribute/GetDeviceAttribute" # Never tested
+_DOOR_STATE_SET_ATTR_NAME = "desireddoorstate"
+_DESIRED_DOOR_STATE_OPEN = 1
+_DESIRED_DOOR_STATE_CLOSED = 0
+
+# Module level constants
+DEVICE_TYPE_GATEWAY = 1
+DEVICE_TYPE_GARAGE_DOOR_OPENER = 2
+
+class MyQ(object):
+
+    # Primary constructor method
+    def __init__(self, userName, password, tokenTTL=600, logger=None):
+
+        # declare instance variables
+        self._userName = userName
+        self._password = password
+        self._tokenTTL = tokenTTL
+        self._securityToken = ""
+        self._lastTokenUpdate = 0
+        self._myQSession = requests.Session()
+        self._myQSession.headers.update({"MyQApplicationId": _APP_ID})
+        
+        if logger is None:
+            # setup basic console logger for debugging
+            logging.basicConfig(format='%(asctime)s %(levelname)s:%(message)s', level=logging.DEBUG)
+            self._logger = logging.getLogger() # Root logger
+        else:
+            self._logger = logger
+
+    # Use the provided login info to obtain a security token
+    def update_security_token(self):
+
+        self._logger.debug("In update_security_token()...")
+
+        params = {
+            "username": self._userName,
+            "password": self._password
+        }
+
+        try:
+            response = self._myQSession.post(
+                "https://{host_uri}/{login_endpoint}".format(
+                    host_uri=_HOST_URI,
+                    login_endpoint=_LOGIN_ENDPOINT
+                ),
+                json=params,
+                timeout=6.05
+            )
+            response.raise_for_status()    # Raise HTTP errors to be handled in exception handling
+
+        # Allow timeout and connection errors to be ignored - log and return false
+        except (requests.exceptions.Timeout, requests.exceptions.ConnectionError, requests.exceptions.HTTPError) as e:
+            self._logger.warn("HTTP POST in update_security_token() failed: %s", str(e))
+            return False
+        except:
+            self._logger.error("Unexpected error occured: %s", sys.exc_info()[0])
+            raise
+
+        authContent = response.json()
+        if "SecurityToken" in authContent:
+            self._securityToken = authContent["SecurityToken"]
+            self._myQSession.headers.update({"SecurityToken": self._securityToken})
+            self._lastTokenUpdate = time.time()
+            self._logger.info("MyQ security token updated")
+            return True
+        else:
+            self._logger.error("Security token not retrieved: %s", authContent["ErrorMessage"])
+
+    # Set the named attribute for the specified device to the specified value
+    def set_device_attribute(self, deviceID, attrName, attrValue):
+
+        self._logger.debug("in set_device_attribute()...")
+
+        # update token if TTL has expired
+        if time.time() > self._lastTokenUpdate + self._tokenTTL:
+            if not self.update_security_token():
+                return False
+
+        payload = {
+            "attributeName": attrName,
+            "myQDeviceId": deviceID,
+            "AttributeValue": attrValue
+        }
+
+        try:
+            response = self._myQSession.put(
+                "https://{host_uri}/{device_set_endpoint}".format(
+                    host_uri=_HOST_URI,
+                    device_set_endpoint=_DEVICE_SET_ATTR_ENDPOINT
+                ),
+                data=payload,
+                timeout=3.05
+            )
+            response.raise_for_status()    # Raise HTTP errors to be handled in exception handling
+
+        # Allow timeout and connection errors to be ignored - log and return false
+        except (requests.exceptions.Timeout, requests.exceptions.ConnectionError, requests.exceptions.HTTPError) as e:
+            self._logger.warn("HTTP PUT in set_device_attribute() failed: %s", str(e))
+            return False
+        except:
+            self._logger.error("Unexpected error occured: %s", sys.exc_info()[0])
+            raise
+        else:
+            return True
+
+    def get_device_list(self):
+    # Get a list of the devices (garage door openers and gateways) in the account
+
+        self._logger.debug("In get_device_list()...")
+
+        # update token if TTL has expired
+        if time.time() > self._lastTokenUpdate + self._tokenTTL:
+            if not self.update_security_token():
+                return None
+
+        try:
+            response = self._myQSession.get(
+                "https://{host_uri}/{device_list_endpoint}".format(
+                    host_uri=_HOST_URI,
+                    device_list_endpoint=_DEVICE_LIST_ENDPOINT
+                ),
+                timeout=6.05
+            )
+            response.raise_for_status()    # Raise HTTP errors to be handled in exception handling
+
+        # Allow timeout and connection errors to be ignored - log and return no data
+        except (requests.exceptions.Timeout, requests.exceptions.ConnectionError, requests.exceptions.HTTPError) as e:
+            self._logger.warn("HTTP PUT in get_device_list() failed: %s", str(e))
+            return None
+        except:
+            self._logger.error("Unexpected error occured: %s", sys.exc_info()[0])
+            raise
+
+        deviceList = []
+        for dev in response.json()["Devices"]:
+
+            deviceID = str(dev["MyQDeviceId"])
+            deviceType = dev["MyQDeviceTypeId"]
+
+            # scan through device attributes and save the important ones
+            for attribute in dev["Attributes"]:
+                if attribute["AttributeDisplayName"] == "desc":
+                    description = attribute["Value"]
+                elif attribute["AttributeDisplayName"] == "online":
+                    online = attribute["Value"] == "True"
+                elif attribute["AttributeDisplayName"] == "numdevices":
+                    numdevices = attribute["Value"]
+                elif attribute["AttributeDisplayName"] == "isunattendedopenallowed":
+                    allow_open = attribute["Value"] == "1"
+                elif attribute["AttributeDisplayName"] == "isunattendedcloseallowed":
+                    allow_close = attribute["Value"] == "1"
+                elif attribute["AttributeDisplayName"] == "doorstate":
+                    state = attribute["Value"]
+                    last_updated = attribute["UpdatedDate"]
+
+            if deviceType == DEVICE_TYPE_GATEWAY: # Gateway
+
+                deviceList.append({
+                    "type":deviceType,
+                    "id": deviceID,
+                    "description": description,
+                    "online": online,
+                    "numdevices": numdevices
+                })
+
+            elif deviceType == DEVICE_TYPE_GARAGE_DOOR_OPENER: # GarageDoorOpener
+
+                deviceList.append({
+                    "type":deviceType,
+                    "id": deviceID,
+                    "description": description,
+                    "state": state,
+                    "last_updated": last_updated,
+                    "allow_open": allow_open,
+                    "allow_close": allow_close
+                })
+
+        return deviceList
+
+    def open(self, deviceID):
+        return self.set_device_attribute(deviceID, _DOOR_STATE_SET_ATTR_NAME, _DESIRED_DOOR_STATE_OPEN)
+
+    def close(self, deviceID):
+        return self.set_device_attribute(deviceID, _DOOR_STATE_SET_ATTR_NAME, _DESIRED_DOOR_STATE_CLOSED)
