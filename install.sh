3ae6e3cd19b055e5a19f35a50f3588bf48730620
W. Randy King <Kingwrandy@gmail.com>
2021-10-06 12:38:12 -0400

    last minute update for Polisy (FreeBSD)

--- install.sh
+++ install.sh
@@ -2,8 +2,8 @@
 
 # package install requirements for Polisy (FreeBSD)
 if [ -f /usr/local/etc/pkg/repos/udi.conf ]; then
-  sudo pkg install libxml2
-  sudo pkg install libxslt
+  pkg install -y libxml2
+  pkg install -y libxslt
 # package install requirements for Raspbian (Debian)
 else
   sudo apt-get install python3-pyquery --quiet --yes
